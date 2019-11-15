8d46b028e112b6dd48d216707df02629034b75b9
Querijn Heijmans <querijn@irule.at>
2019-11-15 11:01:27 +0100

    Add variable frame interval

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 const GameInfo = require("./game_info");
 const fetch = require("node-fetch");
 
-const replayApiUrl = `http://lor.stelar7.no/api/replays/`;
+const replayApiUrl = `http://lor.stelar7.no/api/`;
 const userId = 2;
-const timeInterval = 500;
+const defaultTimeInterval = 500;
 const replay = `hOH5d7qm0WfY2JQccnoCnLwzUuBKryrfNSCAwZJOtQ206RTqGVWuiZ7jgMx8M9AEe9k45FIDjDXesK1Ilv6lTHiGaokUSPbLFNpV`;
 
+let timeInterval = defaultTimeInterval; // Set to default. Update if possible.
+
 async function test() {
+
+	timeIntervalUpdate = await fetch(`http://lor.stelar7.no/api/polling.php?short`);
+	if (timeIntervalUpdate.ok) {
+		try {
+			const text = await timeIntervalUpdate.text();
+			timeInterval = parseInt(text);
+		}
+		catch {
+			timeIntervalUpdate = defaultTimeInterval; // Make sure we haven't got a dummy value.
+		}
+
+		if (timeIntervalUpdate != defaultTimeInterval) {
+			console.log(`Frame interval is ${timeInterval} instead of the default of ${defaultTimeInterval}`);
+		}
+	}
+
 	const fs = require("fs");
 	const gameInfo = new GameInfo();
 
@@ -14,7 +32,7 @@ async function test() {
 	let fetchAttempts = 0;
 	for (let frameIndex = 1; true; frameIndex++) {
 		console.warn(`Loading frame ${frameIndex}..`);
-		const response = await fetch(`${replayApiUrl}/${replay}/${userId}/${timeInterval * frameIndex}.json`);
+		const response = await fetch(`${replayApiUrl}/replays/${replay}/${userId}/${timeInterval * frameIndex}.json`);
 
 		 // TODO: The api needs to tell us either what frames are there or what the last frame is.
 		 // Currently it can skip a frame if it's the same as the previous one
