edf7d7792670b49813aa1a10c3ef262c865cc567
Querijn Heijmans <querijn@irule.at>
2019-11-15 11:48:38 +0100

    Remove some dummy data

--- src/index.js
+++ src/index.js
@@ -1,55 +1,47 @@
 const GameInfo = require("./game_info");
 const fetch = require("node-fetch");
 
-const replayApiUrl = `http://lor.stelar7.no/api/`;
-const userId = 2;
-const defaultTimeInterval = 500;
-const replay = `hOH5d7qm0WfY2JQccnoCnLwzUuBKryrfNSCAwZJOtQ206RTqGVWuiZ7jgMx8M9AEe9k45FIDjDXesK1Ilv6lTHiGaokUSPbLFNpV`;
+const replayApiUrl = `http://lor.stelar7.no/api`;
+const testReplay = `hOH5d7qm0WfY2JQccnoCnLwzUuBKryrfNSCAwZJOtQ206RTqGVWuiZ7jgMx8M9AEe9k45FIDjDXesK1Ilv6lTHiGaokUSPbLFNpV`;
 
-let timeInterval = defaultTimeInterval; // Set to default. Update if possible.
+async function convertReplay(replayUuid) {
 
-async function test() {
+	// TODO: The below needs a user ID.
+	const replayInfoResponse = await fetch(`${replayApiUrl}/replay_by_id.php?id=${replayUuid}`);
+	if (replayInfoResponse.ok == false)
+		throw new Error('Replay not found');
 
-	timeIntervalUpdate = await fetch(`http://lor.stelar7.no/api/polling.php?short`);
-	if (timeIntervalUpdate.ok) {
-		try {
-			const text = await timeIntervalUpdate.text();
-			timeInterval = parseInt(text);
-		}
-		catch {
-			timeIntervalUpdate = defaultTimeInterval; // Make sure we haven't got a dummy value.
-		}
-
-		if (timeIntervalUpdate != defaultTimeInterval) {
-			console.log(`Frame interval is ${timeInterval} instead of the default of ${defaultTimeInterval}`);
-		}
-	}
+	const replayInfo = await replayInfoResponse.json();
+	const timeInterval = parseInt(replayInfo.interval);
+	const userId = replayInfo.user_id;
+	const lastFrameIndex = parseInt(replayInfo.last_frame) / parseInt(replayInfo.interval);
 
 	const fs = require("fs");
 	const gameInfo = new GameInfo();
 
-	let lastFrame = null;
+	let lastFrame = null; // The json of the last frame.
 	let fetchAttempts = 0;
-	for (let frameIndex = 1; true; frameIndex++) {
-		console.warn(`Loading frame ${frameIndex}..`);
-		const response = await fetch(`${replayApiUrl}/replays/${replay}/${userId}/${timeInterval * frameIndex}.json`);
+	for (let frameIndex = 1; frameIndex < lastFrameIndex; frameIndex++) {
+
+		const currentTime = timeInterval * frameIndex;
+		console.warn(`Loading frame ${frameIndex} (time = ${currentTime})..`);
+		const response = await fetch(`${replayApiUrl}/replays/${replayUuid}/${userId}/${currentTime}.json`);
 
 		 // TODO: The api needs to tell us either what frames are there or what the last frame is.
 		 // Currently it can skip a frame if it's the same as the previous one
 		if (response.ok == false) {
-			console.warn(`Could not find frame ${frameIndex} (value: ${timeInterval * frameIndex}, response: ${response.status} ${response.statusText}, attempt: ${fetchAttempts})`);
+			console.warn(`Could not find frame ${frameIndex} (time: ${currentTime}, response: ${response.status} ${response.statusText}, attempt: ${fetchAttempts})`);
 
-			// First frame is not there, or we've tried over 10 times.
-			if (lastFrame == null || fetchAttempts++ > 10)
-				break;
+			if (lastFrame == null) 
+				throw new Error('Unable to fetch last frame!');
 		}
 
 		const json = response.ok ? await response.json() : lastFrame;
-		gameInfo.feedFrame(json);
+		gameInfo.feedFrame(currentTime, json);
 		console.warn(`Loaded frame ${response.ok ? frameIndex : frameIndex - 1}.`);
 		lastFrame = json;
 	}
 
-	fs.writeFileSync(`${replay}_${userId}.json`, gameInfo.asJsonString()); // Write as json string
+	fs.writeFileSync(`${replayUuid}_${userId}.json`, gameInfo.asJsonString()); // Write as json string
 }
-test();
+convertReplay(testReplay);
